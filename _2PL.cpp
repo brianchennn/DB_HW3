69d7cc380b603d9839b5c7444770d98daad0c819
ny40ny40ny <ny40ny40ny.cs07@nctu.edu.tw>
2020-05-27 23:30:10 +0800

    Thread參數搞不定

--- _2PL.cpp
+++ _2PL.cpp
@@ -1,39 +1,52 @@
 #include<iostream>
-#include<pthread.h>
+#include<thread>
 #include<fstream>
-#include<string.h>
 #include<cstring>
 #include<stdio.h>
 #include<cstdlib>
 #include<vector>
 #include <unistd.h>
 #include<semaphore.h>
+#include<mutex>
 using namespace std;
-
-
+vector<mutex*> VSmutex;
+vector<mutex*> VXmutex;
+vector<int> a;
 sem_t semaphore;
-void *threadfunc(void* arg){
-
-	//pthread_exit();
+void threadfunc(vector<int> regs){
+	lock_guard<mutex> XLock(*VXmutex[regs[0]]);
+	for(int i=1;i<regs.size();i++){
+		int reg = regs[i];
+		lock_guard<mutex> SLock(*VSmutex[reg]);
+	}
+	
 }
 
-int main()
+int main(int argc,char *argv[])
 {
 	fstream file;
-	pthread_t t;
-	sem_init(&semaphore,0,0);
+	//thread Thread[stoi(argv[0])];
 	file.open("data/data2",ios::in);
 	int n;file>>n;
-	int a[n];
+	
+	int in;
 	for(int i=0;i<n;i++){
-		file>>a[n];
+		file>>in;
+		a.push_back(in);
+		mutex *SMutex = new(mutex);
+		VSmutex.push_back(SMutex);
+		mutex *XMutex = new(mutex);
+		VXmutex.push_back(XMutex);
 	}
 	string str;
 	string::size_type sz;
-	vector<pair<int,vector<int> > > ReadWrite;
+	vector<vector<int> > ReadWrite;
+	vector<string> line;
+	int counter = 0;
 	while(getline(file,str)){
-		vector<string> v;
-		pair<int,vector<int> > pa;
+		line.push_back(str);
+		
+		vector<int> regs;
 		string tmp="";
 		int flag=0;
 		for(int i=0;i<str.length();i++){
@@ -43,30 +56,22 @@ int main()
 				if(tmp == "=" )flag=1;
 				else if(tmp[0] == '$'){
 					tmp.erase(tmp.begin());
-					if(flag==0)	pa.first = stoi(tmp,&sz);
-					else	    pa.second.push_back(stoi(tmp,&sz));
+					regs.push_back(stoi(tmp));
 				}
 				tmp="";
 			}
-			
 		}
-		v.push_back(tmp);
+		
 		if(tmp[0] == '$'){
 			tmp.erase(tmp.begin());
-			if(flag==0) pa.first = stoi(tmp,&sz);
-			else 		pa.second.push_back(stoi(tmp,&sz));
-			
+			regs.push_back(stoi(tmp));
 		}
-		ReadWrite.push_back(pa);
+		thread Thread(threadfunc,pa);
+		counter++;
+		ReadWrite.push_back(regs);
 	}
 	cout<<ReadWrite.size();
-	for(int i=0;i<ReadWrite.size();i++){
-		cout<<ReadWrite[i].first<<"          ";
-		for(int j=0;j<ReadWrite[i].second.size();j++){
-			cout<<ReadWrite[i].second[j]<<" ";
-		}
-		cout<<endl;
-	}
+	
 
 
 }
