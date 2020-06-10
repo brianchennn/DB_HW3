17cec488b0710f328696a2a5ab77b2c5d1af32c9
ny40ny40ny <ny40ny40ny.cs07@nctu.edu.tw>
2020-06-10 22:44:51 +0800

    add queue

--- HW3_0716211/_2PL.cpp
+++ HW3_0716211/_2PL.cpp
@@ -8,28 +8,19 @@
 #include <unistd.h>
 #include<semaphore.h>
 #include<shared_mutex>
-#include<queue>
 using namespace std;
 vector<shared_mutex*> VSshared_mutex;
 vector<shared_mutex*> VXshared_mutex;
-vector<queue<int> > QV;
-
 vector<int> a;
 sem_t semaphore;
-void threadfunc(vector<int> regs,string str,int line_num){
+void threadfunc(vector<int> regs,string str){
 	//cout<<regs.size()<<endl;
 	//phase 1
-	while(QV[regs[0]].front() != line_num){
-		cout<<"regs[0]="<<regs[0]<<" line_num="<<"L"<<line_num<<endl;
-	}
 	unique_lock<shared_mutex> Xlock(*VSshared_mutex[regs[0]]);
 	//VXshared_mutex[regs[0]]->lock();
 	//VSshared_mutex[regs[0]]->lock();
 	//cout<<"locking X S $"<<regs[0]<<endl;
 	for(int i=1;i<regs.size();i++){
-		while(QV[regs[i]].front() != line_num){
-			cout<<"regs[i]="<<regs[0]<<" line_num="<<"L"<<line_num<<endl;
-		}
 		unique_lock<shared_mutex> Slock(*VSshared_mutex[regs[i]]);
 		//VSshared_mutex[regs[i]]->lock();
 		//cout<<"locking S $"<<regs[i]<<endl;
@@ -78,11 +69,9 @@ void threadfunc(vector<int> regs,string str,int line_num){
 	//phase 2
 	//VXshared_mutex[regs[0]]->unlock();
 	VSshared_mutex[regs[0]]->unlock();
-	QV[regs[0]].pop();
 	//cout<<"unlocking X S $"<<regs[0]<<endl;
 	for(int i=1;i<regs.size();i++){
 		VSshared_mutex[regs[i]]->unlock();
-		QV[regs[i]].pop();
 		//cout<<"locking S $"<<regs[i]<<endl;
 	}
 	
@@ -101,8 +90,6 @@ int main(int argc,char *argv[])
 	
 	int in;
 	for(int i=0;i<n;i++){
-		queue<int> Q;
-		QV.push_back(Q);
 		cin>>in;
 		a.push_back(in);
 		shared_mutex *SMutex = new(shared_mutex);
@@ -113,9 +100,7 @@ int main(int argc,char *argv[])
 	string str;
 	vector<vector<int> > ReadWrite;
 	int counter = 0;
-	int line_num = 0;
 	getline(cin,str);
-	line_num++;
 	while(getline(cin,str)){
 		
 		//cout<<"str="<<str<<endl;
@@ -130,13 +115,13 @@ int main(int argc,char *argv[])
 				else if(tmp[0] == '$'){
 					tmp.erase(tmp.begin());
 					int r = stoi(tmp);
-					bool flag2=0;
+					bool flag=0;
 					for(int i=0;i<regs.size();i++){
-						if(regs[i]==r)flag2=1;
+						if(regs[i]==r)flag=1;
 					}
-					if(flag2==0){
-						regs.push_back(r);
-						QV[r].push(line_num);	
+					if(flag==0){
+						regs.push_back(stoi(tmp));
+						//cout<<tmp<<" ";		
 					}						
 				}
 				tmp="";
@@ -144,21 +129,12 @@ int main(int argc,char *argv[])
 		}
 		if(tmp[0] == '$'){
 			tmp.erase(tmp.begin());
-			int r = stoi(tmp);
-			bool flag2 = 0;
-			for(int i=0;i<regs.size();i++){
-				if(regs[i]==r)flag2=1;
-			}
-			if(flag2==0){
-				regs.push_back(r);
-				QV[r].push(line_num);		
-			}
+			regs.push_back(stoi(tmp));
 		}
-		Threadvector[counter++%stoi(argv[1])] = new thread(threadfunc,regs,str,line_num);// 這裡出問題
+		Threadvector[counter++%stoi(argv[1])] = new thread(threadfunc,regs,str);// 這裡出問題
 		//threadfunc(regs,str);
 		ReadWrite.push_back(regs);
 		//cout<<"\n";
-		line_num++;
 		
 	}
 	for(int i=0;i<stoi(argv[1]);i++){
